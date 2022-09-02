664807aebf717d5e8ae7567c1fae84cb6e0c96c8
kPruehss <kpruehss@oes.edu.au>
2022-09-02 18:07:02 +1000

    Define /getDeals route

--- src/functions/combinationAPI/index.ts
+++ src/functions/combinationAPI/index.ts
@@ -1,23 +1,68 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/apiGateway';
+import Axios from 'axios';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-  const { queryStringParameters = {} } = event;
+  try {
+    const { queryStringParameters = {} } = event;
 
-  const { currency } = queryStringParameters;
+    const { currency } = queryStringParameters;
 
-  if (!currency) {
+    if (!currency) {
+      return formatJSONResponse({
+        statusCode: 400,
+        data: {
+          message: 'Missing currency query parameter',
+        },
+      });
+    }
+
+    const deals = await Axios.get(
+      'https://www.cheapshark.com/api/1.0/deals?upperPrice=15&pageSize=5'
+    );
+
+    const currencyData = await Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${currency}.json`
+    );
+
+    const currencyConversion = currencyData.data[currency];
+
+    const repricedDeals = deals.data.map(deal => {
+      const {
+        title,
+        storeID,
+        salePrice,
+        normalPrice,
+        savings,
+        steamRatingPercent,
+        releaseDate,
+      } = deal;
+
+      return {
+        title,
+        storeID,
+        steamRatingPercent,
+
+        salePrice: salePrice * currencyConversion,
+        normalPrice: normalPrice * currencyConversion,
+        savingsPercent: savings,
+        releaseDate: new Date(releaseDate * 1000).toDateString(),
+      };
+    });
+
     return formatJSONResponse({
-      statusCode: 400,
       data: {
-        message: 'Missing currency query parameter',
+        message: 'success',
+        data: repricedDeals,
+      },
+    });
+  } catch (error) {
+    console.error('error:', error);
+    return formatJSONResponse({
+      statusCode: 502,
+      data: {
+        message: error.message,
       },
     });
   }
-
-  return formatJSONResponse({
-    data: {
-      message: 'success',
-    },
-  });
 };
